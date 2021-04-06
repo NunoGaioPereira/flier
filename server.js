d8bc979c257b07c7f764359036ce41ff1d4c1139
Nuno Pereira <nunocgpereira@gmail.com>
2021-04-06 19:33:46 +0100

    Create register page and style login page

--- server.js
+++ server.js
@@ -19,5 +19,9 @@ app.get('/login', (req, res) => {
 	res.render('login');
 })
 
+app.get('/register', (req, res) => {
+	res.render('register');
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
